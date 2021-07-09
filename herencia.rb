6ef70ddad3fc3b3d1b01f1ec44343e39f81b33c0
poolug <pauluzcategui43@gmail.com>
2021-07-08 23:08:26 -0400

    update herencia.rb

--- herencia.rb
+++ herencia.rb
@@ -1,18 +1,37 @@
 class Appointment
-    attr_accessor :location, :purpose, :hour, :min 
+    attr_reader :location, :purpose, :hour, :min 
     def initialize(location, purpose, hour, min)
+        valid_location_purpose(location, purpose)
+        valid_hour_min(hour, min)
+    end
+    def valid_location_purpose(location, purpose)
         @location = location
         @purpose = purpose
+        if location.class != String || purpose.class != String
+            raise "Debes insertar solo texto para localidad y propuesta"
+        end
+    end
+    def valid_hour_min(hour, min)
         @hour = hour
         @min = min
+        if hour.class != Integer || min.class != Integer
+            raise "Debes insertar sólo números para hora y min"
+        end
     end
 end
 
 class MonthlyAppointment < Appointment
-    attr_accessor :day
+    attr_reader :day
     def initialize(location, purpose, hour, min, day)
         super(location, purpose, hour, min)
+        valid_day(day)
+    end
+
+    def valid_day(day)
         @day = day
+        if day.class != Integer
+            raise "Debes insertar sólo números para el día"
+        end
     end
 
     def occurs_on?(day)
@@ -25,7 +44,6 @@ class MonthlyAppointment < Appointment
 end
 
 class DailyAppointment < Appointment
-    
     def occurs_on?(hour, min)
         self.hour == hour && self.min == min
     end
@@ -39,9 +57,16 @@ class OneTimeAppointment < Appointment
     attr_reader :day, :month, :year
     def initialize(location, purpose, hour, min, day, month, year)
         super(location, purpose, hour, min)
+        valid_date(day, month ,year)
+    end
+
+    def valid_date(day, month ,year)
         @year = year
         @month = month
         @day = day
+        if day.class != Integer || month.class != Integer || year.class != Integer
+            raise "Debes insertar sólo números para el día, mes y año"
+        end
     end
 
     def occurs_on?(day, month, year)
